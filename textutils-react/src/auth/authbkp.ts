90473d50f95889563c48b8dcbfb7a863cd3777c1
dhruv <dhruvyadav17@gmail.com>
2025-12-29 10:54:38 +0530

    commit

--- textutils-react/src/auth/authbkp.ts
+++
@@ -1,7 +0,0 @@
-export const getToken = () => localStorage.getItem("token");
-
-export const clearAuthStorage = () => {
-  localStorage.removeItem("token");
-  localStorage.removeItem("user");
-  localStorage.removeItem("permissions");
-};
